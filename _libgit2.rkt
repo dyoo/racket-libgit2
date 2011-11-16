714801f2aa8248cbbc8d627c55057d128a174006
Danny Yoo <dyoo@cs.wpi.edu>
2011-11-16 15:18:00 -0500

    documenting to myself.

--- _libgit2.rkt
+++ _libgit2.rkt
@@ -10,15 +10,30 @@
 
 
 ;; Let's get git_repository_open and git_repository_free in place.
-
-(define git_repository_open
-  (get-ffi-obj "git_repository_open"
-               libgit2.so
-               (_fun ... ... -> ...)))
-
-;; We'll be getting at the objects using get-ffi-obj, but we need
-;; the proper function signatures next.
-
+;;
+;; We'll be getting at the objects using get-ffi-obj.
+;;
+;; We need the proper function signatures next.
+;; First, let's look at the function signatures in the C API.
+;;
+;; http://libgit2.github.com/libgit2/#HEAD/group/repository/git_repository_open
+;; http://libgit2.github.com/libgit2/#HEAD/group/repository/git_repository_free
+;;
+;; We now need to see how to map the types referenced in the C API to the ones
+;; in Racket's FFI.  These types can be found here:
+;; http://docs.racket-lang.org/foreign/types.html
+;;
+;; We'll touch on the first argument to get_repository_open in a moment.
+;; The second argument to git_repository_open looks like a path, so let's use _path.
+;; The return value of git_repository_open is an _int.
+;;
+;; We now have something that looks like this:
+;; (define git_repository_open
+;;   (get-ffi-obj "git_repository_open"
+;;                 libgit2.so
+;;                 (_fun ... _path -> _int)))
+;;
+;; Ok, let's come back to the question: what do we put in the first part?
+;;
 ;; git_repository_open uses an output parameter for which it writes a
 ;; pointer.  How do we use the ffi with output parameters?
-
